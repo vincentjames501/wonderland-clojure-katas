2916f655932dde8a37319fb17f31892ba629e788
Vincent Pizzo <vincent.pizzo@singlewire.com>
2014-12-29 17:04:47 -0600

    Initial solution for card-game-war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,84 @@
-(ns card-game-war.game)
+(ns card-game-war.game
+  "This kata is a version of the classic card game War.
 
-;; feel free to use these cards or use your own data structure
-(def suits [:spade :club :diamond :heart])
-(def ranks [2 3 4 5 6 7 8 9 :jack :queen :king :ace])
-(def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+   The rules of this card game are quite simple.
 
-(defn play-round [player1-card player2-card])
+   There are two players.
+   The cards are all dealt equally to each player.
+   Each round, player 1 lays a card down face up at the same time that player 2 lays a card down face up. Whoever has
+   the highest value card, wins both round and takes both cards.
+   The winning cards are added to the bottom of the winners deck.
+   Aces are high.
+   If both cards are of equal value, then the winner is decided upon by the highest suit. The suits ranks in order of
+   ascending value are spades, clubs, diamonds, and hearts.
+   The player that runs out of cards loses.")
 
-(defn play-game [player1-cards player2-cards])
+(def ^:private suits
+  "The suit with their corresponding values."
+  {:spades 1 :clubs 2 :diamonds 3 :hearts 4})
+
+(def ^:private ranks
+  "The possible ranks with their corresponding values."
+  (zipmap [:two :three :four :five :six :seven :eight :nine :ten :jack :queen :king :ace] (range 2 15)))
+
+(def ^:private cards
+  "All possible card combinations represented as a map containing two keys: :rank and :suit.
+   e.g. {:rank :two :suit :spade}"
+  (for [suit suits rank ranks]
+    {:suit (key suit) :rank (key rank)}))
+
+(defn- round-details
+  "Builds the round details for a given round of war."
+  [round-number message & [player-1-card player-2-card]]
+  {:round round-number :message message :player-1-card player-1-card :player-2-card player-2-card})
+
+(defn play-round
+  "Compares the two player cards and returns the winner (either :player-1 or :player-2)."
+  [player-1-card player-2-card]
+  (let [player-1-rank-value (get ranks (:rank player-1-card))
+        player-2-rank-value (get ranks (:rank player-2-card))
+        player-1-suit-value (get suits (:suit player-1-card))
+        player-2-suit-value (get suits (:suit player-2-card))]
+    (cond
+      (> player-1-rank-value player-2-rank-value)
+      :player-1
+      (> player-2-rank-value player-1-rank-value)
+      :player-2
+      (> player-1-suit-value player-2-suit-value)
+      :player-1
+      (> player-2-suit-value player-1-suit-value)
+      :player-2
+      :else
+      :player-1)))
+
+(defn play-game
+  "Plays the game of war with either a shuffled deck or predefined cards."
+  ([]
+    (let [shuffled-deck (shuffle cards)
+          [player-1-cards player-2-cards] (split-at 26 shuffled-deck)]
+      (play-game player-1-cards player-2-cards)))
+  ([initial-player-1-cards initial-player-2-cards]
+    (loop [player-1-cards initial-player-1-cards
+           player-2-cards initial-player-2-cards
+           round-number 1
+           rounds []]
+      (cond
+        (empty? player-1-cards)
+        (conj rounds (round-details round-number "Player two wins the game!"))
+
+        (empty? player-2-cards)
+        (conj rounds (round-details round-number "Player one wins the game!"))
+
+        :else
+        (let [player-1-card (first player-1-cards)
+              player-2-card (first player-2-cards)
+              winning-player (play-round player-1-card player-2-card)]
+          (if (= winning-player :player-1)
+            (recur (conj (rest player-1-cards) player-1-card player-2-card)
+                   (rest player-2-cards)
+                   (inc round-number)
+                   (conj rounds (round-details round-number "Player one wins the round!" player-1-card player-2-card)))
+            (recur (rest player-1-cards)
+                   (conj (rest player-2-cards) player-1-card player-2-card)
+                   (inc round-number)
+                   (conj rounds (round-details round-number "Player two wins the round!" player-1-card player-2-card)))))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,37 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higer rank than jacks")
-  (testing "kings are higer rank than queens")
-  (testing "aces are higer rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= :player-2 (play-round {:rank :two :suit :clubs} {:rank :three :suit :clubs})))
+    (is (= :player-1 (play-round {:rank :four :suit :spades} {:rank :three :suit :spades}))))
+  (testing "queens are higer rank than jacks"
+    (is (= :player-1 (play-round {:rank :queen :suit :spades} {:rank :jack :suit :spades})))
+    (is (= :player-2 (play-round {:rank :jack :suit :spades} {:rank :queen :suit :spades}))))
+  (testing "kings are higer rank than queens"
+    (is (= :player-1 (play-round {:rank :king :suit :spades} {:rank :queen :suit :spades})))
+    (is (= :player-2 (play-round {:rank :queen :suit :spades} {:rank :king :suit :spades}))))
+  (testing "aces are higer rank than kings"
+    (is (= :player-1 (play-round {:rank :ace :suit :spades} {:rank :king :suit :spades})))
+    (is (= :player-2 (play-round {:rank :king :suit :spades} {:rank :ace :suit :spades}))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= :player-1 (play-round {:rank :ten :suit :clubs} {:rank :ten :suit :spades})))
+    (is (= :player-2 (play-round {:rank :ten :suit :spades} {:rank :ten :suit :clubs}))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= :player-1 (play-round {:rank :ten :suit :diamonds} {:rank :ten :suit :clubs})))
+    (is (= :player-2 (play-round {:rank :ten :suit :clubs} {:rank :ten :suit :diamonds}))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= :player-1 (play-round {:rank :ten :suit :hearts} {:rank :ten :suit :diamonds})))
+    (is (= :player-2 (play-round {:rank :ten :suit :diamonds} {:rank :ten :suit :hearts})))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (not (empty? (play-game))))
+    (is (= [{:message "Player one wins the round!" :player-1-card {:rank :ace :suit :clubs} :player-2-card {:rank :two :suit :clubs} :round 1}
+            {:message "Player one wins the game!" :player-1-card nil :player-2-card nil :round 2}]
+           (play-game [{:rank :ace :suit :clubs}] [{:rank :two :suit :clubs}])))
+    (is (= [{:message "Player one wins the round!" :player-1-card {:rank :three :suit :hearts} :player-2-card {:rank :three :suit :clubs} :round 1}
+            {:message "Player one wins the round!" :player-1-card {:rank :three :suit :clubs} :player-2-card {:rank :two :suit :clubs} :round 2}
+            {:message "Player one wins the game!" :player-1-card nil :player-2-card nil :round 3}]
+           (play-game [{:rank :three :suit :hearts} {:rank :ace :suit :clubs}] [{:rank :three :suit :clubs} {:rank :two :suit :clubs}])))))
 
